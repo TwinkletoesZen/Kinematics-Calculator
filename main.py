c92d08b747af9364b984774fb40c91bcfba00afb
TwinkletoesZen <contact@twinkletoeszen.com>
2021-03-10 10:35:45 -0800

    Main Functoin Done

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to Free Fall Calculator")
+
+Free_Fall_A = -9.81
+
+print("Enter V0")
+User_Entered_V0 = input(": ")
+
+print("Enter Time")
+User_Entered_Time = input(": ")
+
+print( float(User_Entered_V0) + Free_Fall_A * float(User_Entered_Time))
+
